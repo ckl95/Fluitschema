42f2561b9f6c0fde6f2ed49494b525ffef01c045
Casper Klaver <c.klaver95@gmail.com>
2019-03-17 00:01:50 +0100

    added xlrd

--- helpers.py
+++ helpers.py
@@ -26,6 +26,7 @@ def login_required(f):
     """
     @wraps(f)
     def decorated_function(*args, **kwargs):
+        print(session.get("user_id"))
         if session.get("user_id") is None:
             return redirect("/login")
         return f(*args, **kwargs)
