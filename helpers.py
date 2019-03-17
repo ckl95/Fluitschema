c97c67bdd8d0074003bd86869ca21f059f54e42c
Casper Klaver <c.klaver95@gmail.com>
2019-03-17 14:16:18 +0100

    Changed procfile

--- helpers.py
+++ helpers.py
@@ -17,13 +17,8 @@ def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         print(session.get("user_id"), "user_id")
-        #if session.get("user_id") is None:
-        try: 
-            print(session["user_id"], "session[user_id]")
-            if session["user_id"] is None:
-                return redirect("/login")
-        except:
-            return redirect("/login")
+        if session["user_id"] is None:
+            return redirect("/login")              
         return f(*args, **kwargs)
     return decorated_function
 
