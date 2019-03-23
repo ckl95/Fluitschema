82c162a2134de3f2e5eb9a8f5182c60887b0ee3a
Casper Klaver <c.klaver95@gmail.com>
2019-03-23 12:22:34 +0100

    d

--- database.py
+++ database.py
@@ -12,4 +12,4 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://cmqnzhslytdnff:96d89452b06747de32826f75925a2edb7406b343fcdedd774bb04aec262adf5c@ec2-23-21-165-188.compute-1.amazonaws.com:5432/dkgb8euqaflh"
 database = SQLAlchemy(app)
 
-serve(app, host="127.0.0.1", port=5000)
+serve(app, port=5000)
