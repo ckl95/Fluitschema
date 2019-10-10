4d8630d16eb53d9880adbd62d661d792d044ee96
Casper Klaver <c.klaver95@gmail.com>
2019-10-10 15:09:51 +0200

    support for no date in file

--- to_html.py
+++ to_html.py
@@ -8,86 +8,11 @@ import pandas as pd
 import datetime
 import numpy as np
 
-def format_file(df):
-    """ Puts the newly created pandas dataframe into the right format
-    for to_html_file_writer to work"""
-    
-
-    # Delete unneccesary rows
-    for index, row in df.iterrows():
-        ## Rows not containing a time or a date are deleted 
-        if (type(row[0]) != pd._libs.tslibs.timestamps.Timestamp and type(row[0]) != datetime.datetime) and pd.isna(row[7]) == True:
-            df = df.drop(index)
-    df = df.reset_index(drop=True)
-
-    # If no headers have been given
-    if df.columns[7][0] == "1":
-        df.index = df.index + 1
-        df.loc[0] = df.columns.tolist()
-        df = df.sort_index()
-
-    # Making the first row the headers
-    # With Unnamed: 9, it is expected the whole row is empty
-    # With Unnamed: 8, it is expected the date is on the first row
-    if df.columns[4] == "Unnamed: 4" and (type(df.columns[0]) == pd._libs.tslibs.timestamps.Timestamp or type(df.columns[0]) == datetime.datetime):
-        temp = df.columns[0]
-
-    if df.columns[0] == "Unnamed: 0" or df.columns[4] == "Unnamed: 4":
-        df.columns = df.iloc[0,:].tolist()
-        df = df.drop(0)
-        df = df.reset_index(drop=True)
-        try:
-            df = df.rename(columns={df.columns[0]:temp})
-        except NameError:
-            pass
-        df = df.reset_index(drop=True)
-
-    # Make sure the dates are on the first row of the day
-    for index, row in df.iterrows():
-        if (type(row[0]) == pd._libs.tslibs.timestamps.Timestamp or type(row[0]) == datetime.datetime) and pd.isna(row[7]) == True:
-            df.iloc[index+1,0] = row[0]
-            df.iloc[index,0] = np.nan
-
-    # Delete any more unneccesary rows
-    for index, row in df.iterrows():
-        ## Rows not containing a time or a date are deleted 
-        if (type(row[0]) != pd._libs.tslibs.timestamps.Timestamp and type(row[0]) != datetime.datetime) and pd.isna(row[7]) == True:
-            df = df.drop(index)
-
-    # The first day gets the name of the column if the column is a date
-    if type(df.columns[0]) == pd._libs.tslibs.timestamps.Timestamp or type(df.columns[0]) == datetime.datetime:
-        df.iloc[0,0] = df.columns[0]   
-
-    df = df.reset_index(drop=True)
-
-    # Turns all the time data types into datetime.time
-    ## 1. Turns everything first into a string
-    df.iloc[:,7] = df.iloc[:,7].astype(str)
-    for index, row in df.iterrows():
-        # Prevents 19.3 becoming 19.03
-        if df.iloc[index,7][-2] == ".":
-            df.iloc[index,7] = df.iloc[index,7] + "0"
-
-    ## 2. Then, turns the string into datetime.time
-    for index, row in df.iterrows():
-        try:
-            df.iloc[index,7] = datetime.datetime.strptime(df.iloc[index,7], '%H.%M').time()
-        except ValueError:
-            try:
-                df.iloc[index,7] = datetime.datetime.strptime(df.iloc[index,7], '%H:%M:%S').time()
-            except ValueError:
-                print("Error")
-                exit
-    
-    # Turns NaN into empty strings
-    df = df.fillna('')
-
-    return df
-
-
 def to_html_file_writer(df, f):
     """ Writes the html file out of the pandas dataframe"""
 
+    background_color = "tg-white_row"
+    _insert_headers(df, f, 0)
     # html of the body; filled in per row
     for i in range(len(df)):
 
@@ -100,16 +25,15 @@ def to_html_file_writer(df, f):
                 elif background_color == "tg-green_row":
                     background_color = "tg-white_row"
 
-        if type(df.iloc[i, 0]) == pd._libs.tslibs.timestamps.Timestamp:
-            if i != 0:
+        if type(df.iloc[i, 0]) == pd._libs.tslibs.timestamps.Timestamp and i != 0:
                 f.write("\n</table>\n".encode("utf-8"))
-            background_color = "tg-white_row"
-            _insert_headers(df, f, i)
+                background_color = "tg-white_row"
+                _insert_headers(df, f, i)
 
         ## Table duty formatting
         table = df.iloc[i,3] + ", " + df.iloc[i,4]
         ### Is there a third table duty person assigned?
-        if df.iloc[i,5] != "":
+        if df.iloc[i,5] == "":
             table = table + ", " + df.iloc[i,5]
 
         ## ref formatting
@@ -139,7 +63,11 @@ def to_html_file_writer(df, f):
 def _insert_headers(df, f, i):
     
     # html of the date
-    f.write("""<hr><b><font color = "Green">Tafelschema/Table duty {}</font></b>""".format(df.iloc[i, 0].strftime("%d %B")).encode("utf-8"))
+    try:
+        f.write("""<hr><b><font color = "Green">Tafelschema/Table duty {}</font></b>""".format(df.iloc[i, 0].strftime("%d %B")).encode("utf-8"))
+    # If the date is a string; no present
+    except AttributeError:
+        f.write("""<hr><b><font color = "Green">Tafelschema/Table duty</font></b>""".encode("utf-8"))
 
     # html of the headers
     f.write("""
@@ -151,3 +79,92 @@ def _insert_headers(df, f, i):
     <th class="tg-tacmre">Court Manager</th>
     <th class="tg-tacmre">Referees</th>
   </tr>""".encode("utf-8"))
+
+
+
+def format_file(df):
+    """ Puts the newly created pandas dataframe into the right format
+    for to_html_file_writer to work"""
+    
+    # Delete empty columns and rows
+    df = df.dropna(how='all', axis=1) # Columns
+    df = df.dropna(how='all', axis=0) # Rows
+
+    # Add an empty 'date' column if no dates have been given on the first row or column.
+    bool_1 = df[df.iloc[:,0].apply(lambda x: type(x) == datetime.datetime)].empty
+    bool_2 = df[df.iloc[:,0].apply(lambda x: type(x) == pd._libs.tslibs.timestamps.Timestamp)].empty
+    bool_3 = (type(df.columns[0]) != pd._libs.tslibs.timestamps.Timestamp and type(df.columns[0]) != datetime.datetime)
+    if bool_1 and bool_2 and bool_3:
+        df.insert(0,"Date","")
+
+    # Delete unneccesary rows
+    for index, row in df.iterrows():
+        ## Rows not containing a time and a hometeam are deleted 
+        if (type(row[0]) != pd._libs.tslibs.timestamps.Timestamp and type(row[0]) != datetime.datetime) and pd.isna(row[8]) == True:
+            df = df.drop(index)
+    df = df.reset_index(drop=True)
+
+    # If no headers have been given in xlsx sheet, pandas headers should probably be the first row.
+    if df.columns[7][0] == "1":
+        df.index = df.index + 1
+        df.loc[0] = df.columns.tolist()
+        df = df.sort_index()
+
+    # If pandas headers are an empty row and the xlsx sheet headers are on first row: making the first row the headers
+    ## If date is in the headers, but the table duty header isn't.
+    if df.columns[4] == "Unnamed: 4" and (type(df.columns[0]) == pd._libs.tslibs.timestamps.Timestamp or type(df.columns[0]) == datetime.datetime):
+        temp = df.columns[0]
+
+    ## If no table duty header is present
+    if df.columns[4] == "Unnamed: 4":
+        df.columns = df.iloc[0,:].tolist()
+        df = df.drop(0)
+        df = df.reset_index(drop=True)
+        try:
+            df = df.rename(columns={df.columns[0]:temp})
+        except NameError:
+            pass
+        df = df.reset_index(drop=True)
+
+    # Make sure the dates are on the first row of every day
+    for index, row in df.iterrows():
+        if (type(row[0]) == pd._libs.tslibs.timestamps.Timestamp or type(row[0]) == datetime.datetime) and pd.isna(row[7]) == True:
+            df.iloc[index+1,0] = row[0]
+            df.iloc[index,0] = np.nan
+
+    # Delete any more unneccesary rows
+    for index, row in df.iterrows():
+        ## Rows not containing a time and a hometeam are deleted 
+        if (type(row[0]) != pd._libs.tslibs.timestamps.Timestamp and type(row[0]) != datetime.datetime) and pd.isna(row[8]) == True:
+            df = df.drop(index)
+
+    # The first day gets the name of the column if the column is a date
+    if type(df.columns[0]) == pd._libs.tslibs.timestamps.Timestamp or type(df.columns[0]) == datetime.datetime:
+        df.iloc[0,0] = df.columns[0]
+
+
+    df = df.reset_index(drop=True)
+
+    # Turns all the time data types into datetime.time
+    ## 1. Turns everything first into a string
+    df.iloc[:,7] = df.iloc[:,7].astype(str)
+    for index, row in df.iterrows():
+        # Prevents '19.3' becoming datetime(19.03)
+        if df.iloc[index,7][-2] == ".":
+            df.iloc[index,7] = df.iloc[index,7] + "0"
+
+    ## 2. Then, turns the string into datetime.time
+    for index, row in df.iterrows():
+        try:
+            df.iloc[index,7] = datetime.datetime.strptime(df.iloc[index,7], '%H.%M').time()
+        except ValueError:
+            try:
+                df.iloc[index,7] = datetime.datetime.strptime(df.iloc[index,7], '%H:%M:%S').time()
+            except ValueError:
+                print("Error")
+                exit
+    
+    # Turns NaN into empty strings
+    df = df.fillna('')
+
+    return df
